8761d0bdb21def5a301df79d4f5e2121608c1457
ZN-PC\ZN <jjyynan2008@163.com>
2017-05-03 14:32:09 +0800

    修复微信PC端 无法进行图片混排的问题

--- src/main/webapp/WEB-INF/js/crowdFunding/artEditor.js
+++ src/main/webapp/WEB-INF/js/crowdFunding/artEditor.js
@@ -149,23 +149,30 @@ $.extend($.fn,{
             selection.removeAllRanges();
             selection.addRange(range);
         }
+
+
         if (this._opt.formInputId && $('#' + this._opt.formInputId)[0]) {
             $('#' + this._opt.formInputId).val(this.getValue());
         }
+
     },
     pasteHandler: function () {
-        var _this = this;
         $(this).on("paste", function (e) {
-            console.log(e.clipboardData.items);
-            var content = $(this).html();
-            console.log(content);
-            valiHTML = _this._opt.validHtml;
-            content = content.replace(/_moz_dirty=""/gi, "").replace(/\[/g, "[[-").replace(/\]/g, "-]]").replace(/<\/ ?tr[^>]*>/gi, "[br]").replace(/<\/ ?td[^>]*>/gi, "&nbsp;&nbsp;").replace(/<(ul|dl|ol)[^>]*>/gi, "[br]").replace(/<(li|dd)[^>]*>/gi, "[br]").replace(/<p [^>]*>/gi, "[br]").replace(new RegExp("<(/?(?:" + valiHTML.join("|") + ")[^>]*)>", "gi"), "[$1]").replace(new RegExp('<span([^>]*class="?at"?[^>]*)>', "gi"), "[span$1]").replace(/<[^>]*>/g, "").replace(/\[\[\-/g, "[").replace(/\-\]\]/g, "]").replace(new RegExp("\\[(/?(?:" + valiHTML.join("|") + "|img|span)[^\\]]*)\\]", "gi"), "<$1>");
-            if (!/firefox/.test(navigator.userAgent.toLowerCase())) {
-                content = content.replace(/\r?\n/gi, "<br>");
-            }
-            $(this).html(content);
+            this.selectionRange();
+
+            //console.log(e.clipboardData.items);
+            //var content = $(this).html();
+            //console.log(content);
+            //valiHTML = _this._opt.validHtml;
+            //content = content.replace(/_moz_dirty=""/gi, "").replace(/\[/g, "[[-").replace(/\]/g, "-]]").replace(/<\/ ?tr[^>]*>/gi, "[br]").replace(/<\/ ?td[^>]*>/gi, "&nbsp;&nbsp;").replace(/<(ul|dl|ol)[^>]*>/gi, "[br]").replace(/<(li|dd)[^>]*>/gi, "[br]").replace(/<p [^>]*>/gi, "[br]").replace(new RegExp("<(/?(?:" + valiHTML.join("|") + ")[^>]*)>", "gi"), "[$1]").replace(new RegExp('<span([^>]*class="?at"?[^>]*)>', "gi"), "[span$1]").replace(/<[^>]*>/g, "").replace(/\[\[\-/g, "[").replace(/\-\]\]/g, "]").replace(new RegExp("\\[(/?(?:" + valiHTML.join("|") + "|img|span)[^\\]]*)\\]", "gi"), "<$1>");
+            //if (!/firefox/.test(navigator.userAgent.toLowerCase())) {
+            //    content = content.replace(/\r?\n/gi, "<br>");
+            //}
+            //$(this).html(content);
+
+            _this.selectionRange();
         });
+
     },
     placeholderHandler: function () {
         var _this = this;
@@ -176,7 +183,7 @@ $.extend($.fn,{
         })
             .on('blur', function () {
                 if (!$(this).html()) {
-                    $(this).html(_this._opt.placeholader);
+                    //$(this).html(_this._opt.placeholader);
                 }
             });
 
@@ -189,5 +196,32 @@ $.extend($.fn,{
     },
     setValue: function (str) {
         $(this).html(str);
+    },
+    selectionRange:function(){
+        $(this).focus();
+        var selection = window.getSelection ? window.getSelection() : document.selection;
+        var range = selection.createRange ? selection.createRange() : selection.getRangeAt(0);
+        if (!window.getSelection) {
+            range.pasteHTML(src);
+            range.collapse(false);
+            range.select();
+        } else {
+            range.collapse(false);
+            var hasR = range.createContextualFragment(src);
+            var hasLastChild = hasR.lastChild;
+            while (hasLastChild && hasLastChild.nodeName.toLowerCase() == "br" && hasLastChild.previousSibling && hasLastChild.previousSibling.nodeName.toLowerCase() == "br") {
+                var e = hasLastChild;
+                hasLastChild = hasLastChild.previousSibling;
+                hasR.removeChild(e);
+            }
+            range.insertNode(range.createContextualFragment("<br/>"));
+            range.insertNode(hasR);
+            if (hasLastChild) {
+                range.setEndAfter(hasLastChild);
+                range.setStartAfter(hasLastChild);
+            }
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
     }
 });
